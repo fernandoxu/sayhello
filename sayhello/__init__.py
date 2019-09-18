151155165955d38cc64a97560a92bf5d04d4d3d5
fernandoxu <fernandoxu9@gmail.com>
2019-09-18 16:20:10 +0800

    done

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -1,5 +1,6 @@
-from sayhello import views, errors, commands
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('sayhello')
@@ -8,3 +9,7 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+from sayhello import commands, views, errors
--- sayhello/views.py
+++ sayhello/views.py
@@ -1,23 +1,21 @@
 from flask import flash, redirect, url_for, render_template
 
 from sayhello import app, db
-from sayhello.models import Message
 from sayhello.forms import HelloForm
+from sayhello.models import Message
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # 加载所有记录
-    messages = Message.query.order_by(Message.timestamp.desc()).all()
     form = HelloForm()
     if form.validate_on_submit():
         name = form.name.data
         body = form.body.data
-        message = Message(body=body, name=name)  # 实例化模型类，创建记录
-        db.session.add(message)  # 添加记录到数据库会话
-        db.session.commit()  # 提交会话
+        message = Message(body=body, name=name)
+        db.session.add(message)
+        db.session.commit()
         flash('Your message have been sent to the world!')
         return redirect(url_for('index'))
 
     messages = Message.query.order_by(Message.timestamp.desc()).all()
-    return render_template('index.html', form=form, message=message)
+    return render_template('index.html', form=form, messages=messages)
